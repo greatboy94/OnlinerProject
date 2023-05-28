8fc31097ad359576e91a665c8bff0c8f5efe3d13
unknown <greatboy_94@mail.ru>
2023-05-28 20:45:30 +0500

    Initial commit

---
+++ OnlinerProject/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace OnlinerProject;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
